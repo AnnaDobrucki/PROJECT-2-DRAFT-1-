13d6273a76421d89cbe59e78191605174714e3dc
AnnaDobrucki <anna.dobrucki@hotmail.co.uk>
2023-03-13 10:43:56 +0000

    add button css and functions to increment scores for win and loose

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,27 @@
-let ID = "Boo"
-console.log(ID)
+ function checkGame(){
+    /**
+     * Make sure to come back and add the if - else to the score increase)
+     */
+    scoreWin();
+    scoreLoose();
+ }
+
+
+
+/**
+ * Function for incrementing score for user wins
+ */
+function scoreWin() {
+
+    let scoreWin = parseInt(document.getElementById("win").innerText);
+    document.getElementById("win").innerText = ++scoreWin;
+}
+/**
+ * Function for incrementing score for computer (Nemisis) win
+ */
+
+function scoreWin() {
+
+    let scoreLoose = parseInt(document.getElementById("loose").innerText);
+    document.getElementById("loose").innerText = ++scoreLoose;
+}
